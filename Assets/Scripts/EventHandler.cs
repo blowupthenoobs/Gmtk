537162e0b5ca32d2bdb7028a988fe450904db4e7
blowupthenoobs <88066026+blowupthenoobs@users.noreply.github.com>
2023-07-08 15:29:40 -0400

    Gets Sword On Start

--- Assets/Scripts/EventHandler.cs
+++ Assets/Scripts/EventHandler.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class EventHandler : MonoBehaviour
 {
     public GameObject hero;
+    public GameObject inventory;
+
+
+    //Items
+    public GameObject sword;
 
     void Awake()
     {
         Instantiate(hero, new Vector3(7, -1, 0), Quaternion.Euler(0, 0, 0));
+        inventory.GetComponent<InventoryScript>().recieveItem(sword, 1, false);
     }
 
     void Update()
